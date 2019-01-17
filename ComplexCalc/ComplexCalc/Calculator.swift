504fae9d7e5020ce5fd8e184ec6c3ef573114da1
Conner Gillette <connerwgillette@gmail.com>
2019-01-17 09:48:49 -0800

    Implement basic addition and subtraction methods

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,20 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
+    func add(lhs : Int, rhs : Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func add(_ operands : [Int]) -> Int {
+        var count : Int = 0
+        for operand in operands {
+            count += operand
+        }
+        return count
+    }
+    
+    func subtract(lhs : Int, rhs : Int) -> {
+        return lhs - rhs
+    }
 }
